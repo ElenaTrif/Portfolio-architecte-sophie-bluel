647909cd3cb1d7eb93f9830236f556c781263db6
ElenaTrif <140520572+ElenaTrif@users.noreply.github.com>
2023-12-09 00:48:12 +0100

    Dans le fichier script.js on a fait la fonction pour filtrer les traveaux par catégorie et on a créé les boutons de filtrage

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,23 +1,64 @@
 /********Variables*******/
 const gallery = document.querySelector(".gallery");
+const portfolio = document.getElementById("portfolio");
+const categoryMenu = document.createElement("div");
+    categoryMenu.classList.add("category-menu");
+    portfolio.appendChild(categoryMenu);
+    categoryMenu.after(gallery);
+    console.log(categoryMenu)
 
-// Fonction pour récupérer les traveaux depuis l'API
+
+// (1)Fonction pour récupérer les traveaux depuis l'API
 async function fetchWorks() {
     const reponse = await fetch("http://localhost:5678/api/works");
     return await reponse.json();
 }
 fetchWorks();
 
-// Fonction pour afficher les traveaux dans la galerie
-async function displayWorks() {
-    const listWorks = await fetchWorks();
-    console.log(listWorks);
 
-    // Supprimer les traveaux du HTML déjà présents
+
+// (2)Fonction pour récupérer les catégories depuis l'API
+async function fetchCategories() {
+    const response = await fetch("http://localhost:5678/api/categories");
+    return await response.json();
+}
+
+fetchCategories()
+
+// Fonction pour afficher les catégories dans la category-menu
+async function displayCategories() {
+    const categories = await fetchCategories();
+    const categoryMenu = document.querySelector(".category-menu");
+
+    const allOption = document.createElement("button");
+    allOption.textContent = "Tous";
+    allOption.addEventListener("click", () => filterWorks());
+    categoryMenu.appendChild(allOption);
+
+    categories.forEach((category) => {
+        const categoryItem = document.createElement("button");
+        categoryItem.textContent = category.name;
+        categoryItem.addEventListener("click", () => filterWorks(category.id));
+        categoryMenu.appendChild(categoryItem);
+    });
+}
+
+
+// Fonction pour afficher les travaux dans la galerie
+async function displayWorks(categoryId) {
+    const listWorks = await fetchWorks();
+    const gallery = document.querySelector(".gallery");
+
+    // Supprimer les travaux du HTML déjà présents
     gallery.innerHTML = "";
-    
-    // Ajouter dynamiquement les traveaux
-    listWorks.forEach((work) => {
+
+    // Filtrer les travaux par catégorie si un filtre est spécifié
+    const filteredWorks = categoryId
+        ? listWorks.filter((work) => work.categoryId === categoryId)
+        : listWorks;
+
+    // Ajouter dynamiquement les travaux
+    filteredWorks.forEach((work) => {
         const figure = document.createElement("figure");
         const img = document.createElement("img");
         const figcaption = document.createElement("figcaption");
@@ -32,4 +73,20 @@ async function displayWorks() {
     });
 }
 
-displayWorks();
+
+// Fonction de filtrage des travaux par catégorie
+function filterWorks(categoryId) {
+    displayWorks(categoryId);
+}
+
+
+// Appeler la fonction pour afficher les catégories au chargement de la page
+document.addEventListener("DOMContentLoaded", () => {
+    displayCategories();
+    displayWorks(); // Afficher tous les travaux par défaut
+});
+
+
+
+
+
