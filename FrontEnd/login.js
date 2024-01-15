a29fa6926eddcf3d1639081b9104e8fa9bdf5cb3
ElenaTrif <140520572+ElenaTrif@users.noreply.github.com>
2024-01-15 09:15:34 +0100

    Modifications style.css

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -8,52 +8,52 @@ const loginFailed = document.querySelector(".loginFailed");
 
 // Ajout d'un gestionnaire d'événements pour le formulaire lorsqu'il est soumis
 form.addEventListener("submit", function (event) {
-    // Empêche le comportement par défaut du formulaire (rechargement de la page)
-    event.preventDefault();
+  // Empêche le comportement par défaut du formulaire (rechargement de la page)
+  event.preventDefault();
 
-    // Récupération des valeurs des champs d'e-mail et de mot de passe
-    const emailUser = email.value;
-    const passwordUser = password.value;
+  // Récupération des valeurs des champs d'e-mail et de mot de passe
+  const emailUser = email.value;
+  const passwordUser = password.value;
 
-    // Création de la charge utile au format JSON contenant l'e-mail et le mot de passe
-    const connection = {
-        email: emailUser,
-        password: passwordUser,
-    };
-    const chargeUtile = JSON.stringify(connection);
+  // Création de la charge utile au format JSON contenant l'e-mail et le mot de passe
+  const connection = {
+    email: emailUser,
+    password: passwordUser,
+  };
+  const chargeUtile = JSON.stringify(connection);
 
-    // Appel de la requête - fonction fetch avec toutes les informations nécessaires 
-    fetch("http://localhost:5678/api/users/login", {
-        method: "POST", // Méthode de la requête (POST pour la connexion)
-        headers: { "Content-Type": "application/json" }, // En-tête spécifiant le type de contenu (JSON)
-        body: chargeUtile, // Corps de la requête contenant les données JSON
-    })
+  // Appel de la requête - fonction fetch avec toutes les informations nécessaires 
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST", // Méthode de la requête (POST pour la connexion)
+    headers: { "Content-Type": "application/json" }, // En-tête spécifiant le type de contenu (JSON)
+    body: chargeUtile, // Corps de la requête contenant les données JSON
+  })
     .then((response) => {
       // Vérification de la réussite de la requête
-        if (!response.ok) {
-            // Si la requête échoue, affichage d'un message d'erreur et arrêt du traitement
-          loginFailed.textContent = "Erreur dans l'identifiant ou le mot de passe";
-          loginFailed.style.color = "#FF0000";
-          throw new Error("Erreur dans l'identifiant ou le mot de passe");
-        }
-        return response.json(); // Si la requête réussit, renvoie la réponse au format JSON
-        
-      })
-      .then((data) => {
+      if (!response.ok) {
+        // Si la requête échoue, affichage d'un message d'erreur et arrêt du traitement
+        loginFailed.textContent = "Erreur dans l'identifiant ou le mot de passe";
+        loginFailed.style.color = "#FF0000";
+        throw new Error("Erreur dans l'identifiant ou le mot de passe");
+      }
+      return response.json(); // Si la requête réussit, renvoie la réponse au format JSON
+
+    })
+    .then((data) => {
       // Traitement des données JSON
-        const userId = data.userId;
-        const userToken = data.token;
+      const userId = data.userId;
+      const userToken = data.token;
 
-        // Stockage du token et de l'ID utilisateur dans la session du navigateur
-        window.sessionStorage.setItem("token", userToken);
-        window.sessionStorage.setItem("userId", userId);
+      // Stockage du token et de l'ID utilisateur dans la session du navigateur
+      window.sessionStorage.setItem("token", userToken);
+      window.sessionStorage.setItem("userId", userId);
 
-        // Redirection vers la page "index.html" après la connexion réussie
-        window.location.href = "index.html";
-      })
-      .catch((error) => {
-        // Gestion des erreurs
-        console.error("user not found", error);
-      });
-    
+      // Redirection vers la page "index.html" après la connexion réussie
+      window.location.href = "index.html";
+    })
+    .catch((error) => {
+      // Gestion des erreurs
+      console.error("user not found", error);
+    });
+
 });
